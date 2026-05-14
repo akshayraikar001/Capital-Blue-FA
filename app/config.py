e7d5e055e24b32abf02d6d0a07a229eb95edc1bc
Akshay Raikar <xsabber5@gmail.com>
2026-05-14 23:43:16 +0530

    Move secrets to environment variables

--- app/config.py
+++ app/config.py
@@ -8,11 +8,7 @@ STATIC_DIR = APP_DIR / "static"
 MODEL_DIR = APP_DIR / "ml_models"
 DATABASE_PATH = BASE_DIR / "capitalblue_fastapi.db"
 
-FINNHUB_API_KEY = os.getenv(
-    "FINNHUB_API_KEY",
-    "d0q7ta9r01qt60omn54gd0q7ta9r01qt60omn550",
-)
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY", "")
 SESSION_SECRET = os.getenv("SESSION_SECRET", "capitalblue-fastapi-session-secret")
 
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
-
